fa7cb2fbebde0f3ad41482996b77d63df795b9a3
Bagus Setyawan <bagussetyawan551@gmail.com>
2023-06-24 20:28:41 +0700

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,18 +11,101 @@
 <div class="row">
     <div class="col-12">
         <div class="page-title-box d-flex align-items-center justify-content-between">
-            <h4 class="mb-0">Starter Page</h4>
+            <h2 class="mb-0">Dashboard</h2>
 
-            <div class="page-title-right">
-                <ol class="breadcrumb m-0">
-                    <li class="breadcrumb-item"><a href="javascript: void(0);">Utility</a></li>
-                    <li class="breadcrumb-item active">Starter Page</li>
-                </ol>
-            </div>
+            
 
         </div>
     </div>
 </div>
+
+<div class="row">
+    <div class="col-md-6 col-xl-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="float-end mt-2">
+                    <div id="total-revenue-chart" data-colors='["--bs-primary"]'></div>
+                </div>
+                <div>
+                    <h4 class="mb-1 mt-1"><span data-plugin="counterup">10</span></h4>
+                    <p class="text-muted mb-0">Total Alternatif</p>
+                </div>
+                {{-- <p class="text-muted mt-3 mb-0"><span class="text-success me-1"><i class="mdi mdi-arrow-up-bold me-1"></i>2.65%</span> since last week
+                </p> --}}
+            </div>
+        </div>
+    </div> <!-- end col-->
+
+    <div class="col-md-6 col-xl-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="float-end mt-2">
+                    <div id="orders-chart" data-colors='["--bs-success"]'> </div>
+                </div>
+                <div>
+                    <h4 class="mb-1 mt-1"><span data-plugin="counterup">5</span></h4>
+                    <p class="text-muted mb-0">Total Kriteria</p>
+                </div>
+                {{-- <p class="text-muted mt-3 mb-0"><span class="text-danger me-1"><i class="mdi mdi-arrow-down-bold me-1"></i>0.82%</span> since last week
+                </p> --}}
+            </div>
+        </div>
+    </div> <!-- end col-->
+
+    <div class="col-md-6 col-xl-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="float-end mt-2">
+                    <div id="customers-chart" data-colors='["--bs-primary"]'> </div>
+                </div>
+                <div>
+                    <h4 class="mb-1 mt-1"><span data-plugin="counterup">0</span></h4>
+                    <p class="text-muted mb-0">Hasil WP</p>
+                </div>
+                {{-- <p class="text-muted mt-3 mb-0"><span class="text-danger me-1"><i class="mdi mdi-arrow-down-bold me-1"></i>6.24%</span> since last week
+                </p> --}}
+            </div>
+        </div>
+    </div> <!-- end col-->
+
+    <div class="col-md-6 col-xl-3">
+
+        <div class="card">
+            <div class="card-body">
+                <div class="float-end mt-2">
+                    <div id="growth-chart" data-colors='["--bs-warning"]'></div>
+                </div>
+                <div>
+                    <h4 class="mb-1 mt-1"> <span data-plugin="counterup">0</span></h4>
+                    <p class="text-muted mb-0">Hasil Topsis</p>
+                </div>
+                {{-- <p class="text-muted mt-3 mb-0"><span class="text-success me-1"><i class="mdi mdi-arrow-up-bold me-1"></i>10.51%</span> since last week
+                </p> --}}
+            </div>
+        </div>
+    </div> <!-- end col-->
+</div> <!-- end row-->
+
+
+<div class="col-lg-7">
+    <div class="card bg-info border-info text-white-50">
+        <div class="card-body">
+            <h5 class="mb-4 text-white"><i class="uil uil-question-circle me-3"></i>Panduan:</h5>
+            <p class="card-text text-white-50">1. Menu Users : menu untuk mengelola user yang menggunakan aplikasi.</p>
+            <p class="card-text text-white-50">2. Menu Alternatif : menu untuk menentukan alternatif (spesifikasi komputer) alternatif adalah pilihan atau objek yang disarankan.</p>
+            <p class="card-text text-white-50">3. Menu Kriteria : menu untuk mengelola kriteria yang digunakan untuk menilai.</p>
+            <p class="card-text text-white-50">4. Menu Hasil WP : menu yang digunakan untuk melihat hasil perhitungan dari metode weighted product.</p>
+            <p class="card-text text-white-50">5. Menu Hasil TOPSIS : menu yang digunakan untuk melihat hasil perhitungan dari metode TOPSIS.</p>
+
+
+
+        </div><!-- end card-body -->
+    </div><!-- end card -->
+</div><!-- end col-md-4 -->
+</div>
+<!-- end row -->
+
+    </div> <!-- end col-->
 <!-- end page title -->
 @endsection
 @section('script')
