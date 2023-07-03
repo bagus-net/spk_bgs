2850eccb5378006f949b2ff9b966cc5982966e29
bagus-net <bagussetyawan551@gmail.com>
2023-07-03 09:36:35 +0700

    dumpdb2

---
+++ db/db_spklaravel2.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : bagus
+ Source Server Type    : MySQL
+ Source Server Version : 50742
+ Source Host           : localhost:3306
+ Source Schema         : db_spklaravel
+
+ Target Server Type    : MySQL
+ Target Server Version : 50742
+ File Encoding         : 65001
+
+ Date: 01/07/2023 12:49:58
+*/
+
